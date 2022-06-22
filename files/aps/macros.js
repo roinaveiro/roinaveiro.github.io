7eaa52612ebf22c6ebfc34a64a1870b6e62cd83c
roinaveiro <roi.naveiro@gmail.com>
2022-06-23 00:57:17 +0200

    fiz

--- files/aps/macros.js
+++
@@ -1,8 +0,0 @@
-remark.macros.scale = function (percentage) {
-  var url = this;
-  return '<img src="' + url + '" style="width: ' + percentage + '" />';
-};
-
-remark.macros.vspace = function (percentage) {
-  return `<div style="height: ${percentage}%"></div>`;
-};
